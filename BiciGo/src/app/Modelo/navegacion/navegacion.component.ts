2a49ffd72205a7d959433a727cc3ba3c91e24767
george yaguana <sebyaguana@gmail.com>
2023-09-05 10:42:14 -0500

    actualizar informacion

--- BiciGo/src/app/Modelo/navegacion/navegacion.component.ts
+++ BiciGo/src/app/Modelo/navegacion/navegacion.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SessionService } from '../../Servicios/session.service';
+import { usuarios } from '../../Interfaces/usuarios';
 
 @Component({
   selector: 'app-navegacion',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./navegacion.component.css']
 })
 export class NavegacionComponent {
+  public usuario: usuarios | null = null;
+  mostrarNavegacion = true;
+  
+  constructor(private sessionService: SessionService) { 
+    
+  }
 
+  ngOnInit(): void {
+    this.usuario = this.sessionService.getGlobalElement();
+    if (this.usuario && this.usuario.tipo_usuario === 'normal') {
+      this.mostrarNavegacion = false;
+    }
+  }
 }
+
+
+
+
