fc22c733d44bac1af8b92bab625b99e8baa9ddf2
JosueRodriguezr <peroreye@espol.edu.ec>
2023-09-04 20:00:49 -0500

    Crear Ruta

--- BiciGo/src/app/app-routing.module.ts
+++ BiciGo/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { CrearRutaComponent } from './crear-ruta/crear-ruta.component';
 import { RegisterComponent } from './register/register.component';
 import { ProfileComponent } from './profile/profile.component';
 import { FeedComponent  } from './Modelo/feed/feed.component';
@@ -14,6 +15,7 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'profile', component: ProfileComponent },
+  { path: 'crear-ruta', component: CrearRutaComponent },
   {path: 'feed', component:FeedComponent}
 ];
 
