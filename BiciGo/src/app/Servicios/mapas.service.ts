bf3b1c71147e5e1824062630b368922e97f4b14a
JosueRodriguezr <peroreye@espol.edu.ec>
2023-09-05 13:06:59 -0500

    Rutas

--- BiciGo/src/app/Servicios/mapas.service.ts
+++ BiciGo/src/app/Servicios/mapas.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -8,10 +8,24 @@ export class MapasService {
 
 
   private URL: string = 'http://localhost/apiMapa/get/';
-
+  private apiUrl = 'http://localhost/apiMapa/put/';
+  constructor(private http: HttpClient) { }
   getResponse() {
     return this.http.get(this.URL);
-}
-  constructor(private http:HttpClient) { }
+  }
+  
 
+  agregarRuta(puntoPartida1: string, puntoPartida2: string, nombrePuntoPartida: string, nombrePuntoFinal: string, lider: string) {
+    //PuntoPartida1,PuntoFinal1,PuntoPartida2,PuntoFinal2,NombrePuntoPartida,NombrePuntoFinal,lider
+    const formData = new FormData();
+    formData.append('PuntoPartida1', puntoPartida1);
+    formData.append('PuntoPartida2', puntoPartida2);
+    formData.append('NombrePuntoPartida', nombrePuntoPartida);
+    formData.append('NombrePuntoFinal', nombrePuntoFinal);
+    formData.append('lider', lider);
+    const headers = new HttpHeaders();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+
+    return this.http.post(this.apiUrl, formData, { headers });
+  }
 }
--- BiciGo/src/app/crear-ruta/crear-ruta.component.ts
+++ BiciGo/src/app/crear-ruta/crear-ruta.component.ts
@@ -1,26 +1,87 @@
 import { Component } from '@angular/core';
-import {MapasService} from '../Servicios/mapas.service'
-export interface mapa{
-  PuntoPartida: string
-  PuntoFinal: string
-  NombrePuntoPartida: string
-  NombrePuntoFinal: string
-  lider: string
+import { HttpClient } from '@angular/common/http';
+import { MapasService } from '../Servicios/mapas.service';
+
+export interface mapa {
+  PuntoPartida: string;
+  PuntoFinal: string;
+  NombrePuntoPartida: string;
+  NombrePuntoFinal: string;
+  lider: string;
 }
+
 @Component({
   selector: 'app-crear-ruta',
   templateUrl: './crear-ruta.component.html',
-  styleUrls: ['./crear-ruta.component.css']
+  styleUrls: ['./crear-ruta.component.css'],
 })
 export class CrearRutaComponent {
-  username: string = '';
-  password: string = '';
-  public data1: mapa[] = [];
+  direccionInicio: string = '';
+  direccionFin: string = '';
+  coordenadasInicio: any = null;
+  coordenadasFin: any = null;
+  errorInicio: string = '';
+  errorFin: string = '';
+  organizador: string = '';
 
-  constructor(private mapaService: MapasService) { }
-  ngOnInit() {
-    this.mapaService.getResponse().subscribe((response) => {
-      this.data1 = (response as mapa[]);
-    });
+  constructor(private http: HttpClient, private mapasService: MapasService) {}
+
+  obtenerCoordenadasInicio() {
+    const apiUrl = 'http://localhost:3000/direccion?direccion=' + encodeURIComponent(this.direccionInicio);
+
+    this.http.get(apiUrl).subscribe(
+      (data: any) => {
+        if (data.coordenadas) {
+          this.coordenadasInicio = data.coordenadas;
+          this.errorInicio = '';
+        } else {
+          this.coordenadasInicio = null;
+          this.errorInicio = 'Coordenadas no encontradas para la dirección proporcionada';
+        }
+      },
+      (error) => {
+        console.error(error);
+        this.coordenadasInicio = null;
+        this.errorInicio = 'Ocurrió un error al obtener las coordenadas';
+      }
+    );
+  }
+
+  obtenerCoordenadasFin() {
+    const apiUrl = 'http://localhost:3000/direccion?direccion=' + encodeURIComponent(this.direccionFin);
+
+    this.http.get(apiUrl).subscribe(
+      (data: any) => {
+        if (data.coordenadas) {
+          this.coordenadasFin = data.coordenadas;
+          this.errorFin = '';
+        } else {
+          this.coordenadasFin = null;
+          this.errorFin = 'Coordenadas no encontradas para la dirección proporcionada';
+        }
+      },
+      (error) => {
+        console.error(error);
+        this.coordenadasFin = null;
+        this.errorFin = 'Ocurrió un error al obtener las coordenadas';
+      }
+    );
+  }
+
+  obtenerCoordenadas(){
+    this.obtenerCoordenadasFin();
+    this.obtenerCoordenadasInicio();
+  }
+
+  onSubmit(){
+    this.mapasService.agregarRuta(this.coordenadasInicio, this.coordenadasFin, this.direccionInicio, this.direccionFin, this.organizador)
+      .subscribe(
+        (response) => {
+          console.log(response);
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
   }
 }
